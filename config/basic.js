228c6745485a5ea8f85c90da08535dccd575c786
bandazhenxin <webbandazhenxin@163.com>
2019-07-04 11:32:24 +0800

    few

--- config/basic.js
+++ config/basic.js
@@ -8,7 +8,7 @@ let basic = {
   accuracy:    65,
   qqmap_key:   '5SDBZ-NB4K2-Q3TUP-CPO4A-HX3JO-VNBKF',
   province:    '上海',
-  city: '      上海市',
+  city:        '上海市',
   letter:      ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
   linkPhone:   '020-22042931',
 };
